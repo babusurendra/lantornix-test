199a3bb8b031071f94c388828c4813baa9da93f7
babusurendra <babusurendra.p@gmail.com>
2020-07-06 22:39:16 +0530

    Add files via upload
    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcrypt');
+const { User, validate } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+ 
+router.post('/register', async (req, res) => {
+    // First Validate The Request
+    const { error } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+ 
+    // Check if this user already exisits
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+        return res.status(400).send('That user already exisits!');
+    } else {
+        // Insert the new user if they do not exist yet
+        user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        await user.save();
+        res.send(user);
+    }
+});
+router.post('/login', async (req, res) => {
+    // First Validate The HTTP Request
+    const { error } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+ 
+    //  Now find the user by their email address
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).send('Incorrect email or password.');
+    }
+ 
+    // Then validate the Credentials in MongoDB match
+    // those provided in the request
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+        return res.status(400).send('Incorrect email or password.');
+    }
+    const token = jwt.sign({ _id: user._id }, 'PrivateKey');
+    res.send(token);
+});
+ 
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+ 
+    return Joi.validate(req, schema);
+}
+ 
+module.exports = router;
